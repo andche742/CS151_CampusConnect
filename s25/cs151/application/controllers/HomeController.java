edfe1d792663508ec86d8468f89c84beffbb506d
rkumar101360 <151259786+rkumar101360@users.noreply.github.com>
2025-03-14 17:35:59 -0700

    Update HomeController.java

--- s25/cs151/application/controllers/HomeController.java
+++ s25/cs151/application/controllers/HomeController.java
@@ -1,9 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.io.IOException;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Home");
+        Pane root = fxmlLoader.load();
+        root.setStyle("-fx-background-color: blue;");
+
+        // Add "Campus Connect" label at the top left
+        Label titleLabel = new Label("Campus Connect");
+        titleLabel.setStyle("-fx-font-size: 18px; -fx-text-fill: white; -fx-font-weight: bold;");
+        titleLabel.setLayoutX(10);
+        titleLabel.setLayoutY(10);
+        root.getChildren().add(titleLabel);
+
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Campus Connect");
         stage.setScene(scene);
         stage.show();
     }
@@ -12,3 +30,4 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
