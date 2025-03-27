cddb3a30c04476873f85112cd070299b4b247430
meeraa5 <vmeeraa5@gmail.com>
2025-03-26 21:00:31 -0700

    Update Courses.java

--- src/main/java/s25/cs151/application/models/Courses.java
+++ src/main/java/s25/cs151/application/models/Courses.java
@@ -1,30 +1,118 @@
 package s25.cs151.application.models;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Model class for Courses.
+ * Represents a course with code, name, and section number.
+ */
 public class Courses {
-
-
-    // course code
-    // course name
-    // section number
-
-    private String courseName;
-    private String courseCode;
-    private String section;
-
-    public Courses(String courseName, String courseCode, String section) {
-        this.courseName = courseName;
-        this.courseCode = courseCode;
-        this.section = section;
+    private final StringProperty courseCode;
+    private final StringProperty courseName;
+    private final StringProperty sectionNumber;
+    
+    /**
+     * Constructor with all properties.
+     * 
+     * @param courseCode The course code (e.g., CS151)
+     * @param courseName The course name (e.g., Object-Oriented Design)
+     * @param sectionNumber The section number (e.g., 01)
+     */
+    public Courses(String courseCode, String courseName, String sectionNumber) {
+        this.courseCode = new SimpleStringProperty(courseCode);
+        this.courseName = new SimpleStringProperty(courseName);
+        this.sectionNumber = new SimpleStringProperty(sectionNumber);
     }
-
-    public String getCourseName() {
-        return courseName;
-    }
+    
+    /**
+     * Get the course code.
+     * 
+     * @return The course code
+     */
     public String getCourseCode() {
+        return courseCode.get();
+    }
+    
+    /**
+     * Set the course code.
+     * 
+     * @param courseCode The course code to set
+     */
+    public void setCourseCode(String courseCode) {
+        this.courseCode.set(courseCode);
+    }
+    
+    /**
+     * Get the course code property.
+     * 
+     * @return The course code property
+     */
+    public StringProperty courseCodeProperty() {
         return courseCode;
     }
-    public String getSection() {
-        return section;
+    
+    /**
+     * Get the course name.
+     * 
+     * @return The course name
+     */
+    public String getCourseName() {
+        return courseName.get();
     }
-
+    
+    /**
+     * Set the course name.
+     * 
+     * @param courseName The course name to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName.set(courseName);
+    }
+    
+    /**
+     * Get the course name property.
+     * 
+     * @return The course name property
+     */
+    public StringProperty courseNameProperty() {
+        return courseName;
+    }
+    
+    /**
+     * Get the section number.
+     * 
+     * @return The section number
+     */
+    public String getSectionNumber() {
+        return sectionNumber.get();
+    }
+    
+    /**
+     * Set the section number.
+     * 
+     * @param sectionNumber The section number to set
+     */
+    public void setSectionNumber(String sectionNumber) {
+        this.sectionNumber.set(sectionNumber);
+    }
+    
+    /**
+     * Get the section number property.
+     * 
+     * @return The section number property
+     */
+    public StringProperty sectionNumberProperty() {
+        return sectionNumber;
+    }
+    
+    /**
+     * String representation of a course.
+     * 
+     * @return String representation
+     */
+    @Override
+    public String toString() {
+        return courseCode.get() + " - " + courseName.get() + " (Section " + sectionNumber.get() + ")";
+    }
 }
